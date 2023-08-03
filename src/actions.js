959974b4f2a0b57dee9db0fae8721bce40e6bc4b
rnutall <rnutall1@gmail.com>
2023-08-03 01:03:07 -0500

    added index.js, actions.js, reducer.js

---
+++ src/actions.js
@@ -0,0 +1,10 @@
+import * as actions from './actionTypes';
+
+export function bugAdded(description) {
+    return {
+        type: action.BUG_REMOVED,
+    payload: {
+        description: 'Bug1'
+    }
+};
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+import store from './store';
+
+//state = reducer(state, action);
+//notify the subscribers
+const unsubscribe = store.subscribe(() => {
+    console.log('Store Changed!', store.getState());
+});
+
+store.dispatch ({
+    type: action.BUG_ADDED,
+    payload: {
+        description: 'Bug1'
+    }
+});
+unsubscribe();
+
+store.dispatch({
+    type: action.BUG_REMOVED,
+    payload: {
+        description: 'Bug1'
+    }
+});
+    
+
+console.log(store.getState());
